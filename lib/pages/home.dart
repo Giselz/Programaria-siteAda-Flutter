7f7676e434082770fb0aa11121e15991b2f40fe4
Gisele Santos <giseledosantos@outlook.com>
2022-08-03 22:48:32 -0300

    implementacao do design na tela inicial

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,44 +18,88 @@ class _HomePageState extends State<HomePage>{
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Stack(
+                Stack(
                 children: <Widget>[
-                  Image.asset("assets/ada_lovelace_full.png"),
+                  Center(
+                    child: ShaderMask(
+                      shaderCallback: (Rect bounds) { 
+                        return const LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.white,
+                            Colors.black
+                          ]
+                          ).createShader(bounds);
+                      },
+                      child: Image.asset("assets/ada_lovelace.jpg",
+                      width: 700.0,
+                      height: 718.0,
+                      fit: BoxFit.cover,),
+                    ),
+                  ),
                   Column(
                     children: [
                       Padding(
-                  padding: const EdgeInsets.only(top: 250.0),
-                  child: Text('Mulheres\nque fizeram\nhistória na tecnologia' .toUpperCase(), textAlign: TextAlign.center, 
-                  style: const TextStyle(color: Colors.white, fontSize: 25.0)),
+                        padding: const EdgeInsets.only(top: 380.0),
+                        child: Text('Mulheres\nque fizeram\nhistória na tecnologia' .toUpperCase(), textAlign: TextAlign.center, 
+                        style: const TextStyle(color: Colors.white, fontSize: 25.0, fontWeight: FontWeight.w900,),
+                        ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.all(30.0),
-                  child: Text('Conheça algumas das mulheres que fizeram história na tecnologia.', textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white, fontSize: 20.0)),
-                  ),
-                SizedBox(
-                  width: 230.0,
-                  height: 35.0,
-                  child: ElevatedButton(
-                    onPressed: () {}, 
-                    child: const Text('Começar'), 
-                    style: ElevatedButton.styleFrom(primary: Color.fromARGB(255, 64, 5, 92))),
-                )
+                  const Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Text('Conheça algumas mulheres que fizeram história na tecnologia.', textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                    ),
+                  Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 70.0),
+                          child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                gradient: const LinearGradient(
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                  stops: [0.0, 8.0],
+                                  colors: [
+                                    Color.fromARGB(255, 194, 168, 19),
+                                    Color.fromARGB(255, 206, 203, 35)
+                                  ],
+                                ),
+                                borderRadius: BorderRadius.circular(80.0),
+                              ),
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(80.0),
+                                    ),
+                                  ),
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Colors.transparent),
+                                ),
+                                onPressed: () {},
+                                child: const SizedBox(
+                                  width: double.maxFinite,
+                                  height: 50.0,
+                                  child: Center(
+                                    child: Text(
+                                      'Começar',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20.0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )),
+                        ),
             ],
               ),
             ],
               ),
                     ],
                   ),
-                  /*
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0),
-                child: Image.asset("assets/ada_lovelace.jpeg", 
-                width: 300.0,
-                height: 300.0,
-                ),
-                ),*/
-                
           ),
         ),
       );
